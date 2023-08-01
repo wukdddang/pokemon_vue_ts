d16666c2b10a8354e1948e7dc70a5534b2812997
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-08-01 16:05:52 +0900

    Feat: PokemonOptions 컴포넌트 추가

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="options-container">
+    <ul>
+      <li v-for="pokemon in pokemons"
+          :key="pokemon.id"
+          @click="$emit( 'selectionPokemon', pokemon.id )"
+      >
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+import { Pokemon } from '../interfaces/pokemon'
+
+defineProps<{
+  pokemons: Pokemon[]
+}>()
+
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+}
+
+li:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
