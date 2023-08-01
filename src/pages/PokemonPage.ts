7a418eb333093254804ba0c953c951a7af2b45ac
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-08-01 16:03:28 +0900

    Feat: PokemonPage 컴포넌트 추가 - 프레젠테이션 컴포넌트 구현 - .ts 파일에서 구현한 내용을 .vue 파일에 적용

---
+++ src/pages/PokemonPage.ts
@@ -0,0 +1,72 @@
+import { defineComponent, ref } from 'vue'
+import PokemonOptions from '../components/PokemonOptions.vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
+
+import getPokemonOptions from '../helpers/getPokemonOptions'
+import { Pokemon } from '../interfaces/pokemon'
+
+export default defineComponent({
+  name: 'PokemonPage',
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+  setup: () => {
+    const pokemonArr = ref<Pokemon[]>([])
+    const pokemon = ref<Pokemon>()
+    const showPokemon = ref(false)
+    const showAnswer = ref(false)
+    const message = ref('');
+
+    let retryCount = 0;
+    const mixPokemonArray = async () => {
+      try {
+        pokemonArr.value = await getPokemonOptions()
+      } catch (error) {
+        if (retryCount < 3) {
+          retryCount++;
+          mixPokemonArray()
+        }
+        return;
+      }
+      retryCount = 0;
+      const randomInt = Math.floor(Math.random() * 4)
+      pokemon.value = pokemonArr.value[randomInt]
+    }
+
+    const checkAnswer = ( selectedId: number) => {
+      if (!pokemon.value) return;
+
+      showPokemon.value = true
+      showAnswer.value = true
+
+      if (selectedId === pokemon.value.id) {
+        message.value = `맞았습니다! ${pokemon.value.name}입니다.`
+      } else {
+        message.value = `틀렸습니다! 정답은 ${pokemon.value.name}입니다.`
+      }
+    }
+
+    const newGame = () => {
+      showPokemon.value = false
+      showAnswer.value = false
+      pokemonArr.value = [];
+      pokemon.value = undefined;
+      mixPokemonArray()
+    }
+
+    mixPokemonArray();
+
+    return {
+      pokemonArr,
+      pokemon,
+      showPokemon,
+      showAnswer,
+      message,
+
+      mixPokemonArray,
+      checkAnswer,
+      newGame,
+    }
+  }
+})
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<script src="./PokemonPage.ts" />
+<template>
+  <h1 v-if="!pokemon">기다려주세요...</h1>
+  <div v-else>
+    <h1>이 포켓몬은 무엇일까요?</h1>
+    <PokemonPicture
+      :pokemonId="pokemon.id"
+      :showPokemon="showPokemon"
+    />
+    <PokemonOptions
+      :pokemons="pokemonArr"
+      @selection-pokemon="checkAnswer"
+    />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">
+        새로운 게임
+      </button>
+    </template>
+  </div>
+</template>
